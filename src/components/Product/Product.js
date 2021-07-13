22c77137a53e7567e43a9506beef78d1842caf66
iqbal <iqbaldb32@gmail.com>
2021-07-14 01:29:09 +0600

    product list added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
+    console.log(props.product);
+    const {img, name, seller, price, stock} = props.product;
     return (
-        <div>
-            <h3>{props.product.name}</h3>
+        <div className="product">
+            <div className="product-img">
+                <img src={img} alt="" />
+            </div>
+            <div className="product-name">
+                <h4>{name}</h4>
+                <br />
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
+            </div>
+
         </div>
+        
     );
 };
 

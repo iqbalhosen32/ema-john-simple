d3e5551730d82f6e20ff42583b88dc19d34b7324
iqbal <iqbaldb32@gmail.com>
2021-07-15 00:42:01 +0600

    cart added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const total = cart.reduce((total, pd) => total + pd.price, 0);
+
+    let shipping = 0;
+    if(total > 350){
+        shipping = 0;
+    }
+    else if(total > 150){
+        shipping = 4.99;
+    }
+    else if(total > 100){
+        shipping = 7.99;
+    }
+    else if(total > 50){
+        shipping = 12.99;
+    }
+
+    const tax = total*0.10;
+
+    const grandTotal = total + tax + shipping;
+
+    const formatNumber = num =>{
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+    return (
+        <div>
+            <h3>This is cart</h3>
+            <p>Items Ordered: {cart.length}</p>
+            <p><small>Product Price: {formatNumber(total)}</small></p>
+            <p><small>Shipping Cost: {formatNumber(shipping)}</small></p>
+            <p><small>Vat + Tax: {formatNumber(tax)}</small></p>
+            <p>Total Price: {formatNumber(grandTotal)}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import FakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10)
     const [products, setState] = useState(first10)
+    const [cart, setCart] = useState([])
     const handleAddProduct = (product) =>{
-        console.log('product added', product)
+        // console.log('product added', product)
+        const newCart = [...cart, product];
+        setCart(newCart);
+        console.log(setCart(newCart))
     }
     return (
         <div className="shop-container">
@@ -21,7 +26,7 @@ const Shop = () => {
                     }
             </div>
             <div className="cart-contsiner">
-                <h2>This is cart</h2>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );

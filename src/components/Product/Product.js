5ba2b2c933b6f173d082996aae2b5f444c77d0e1
iqbal <iqbaldb32@gmail.com>
2021-07-14 12:49:43 +0600

    click handle added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-    console.log(props.product);
+    // console.log(props);
     const {img, name, seller, price, stock} = props.product;
     return (
         <div className="product">
@@ -15,6 +17,7 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <button className="cart-button" onClick={() => props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
             </div>
 
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,14 +7,18 @@ import './Shop.css'
 const Shop = () => {
     const first10 = fakeData.slice(0,10)
     const [products, setState] = useState(first10)
+    const handleAddProduct = (product) =>{
+        console.log('product added', product)
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
-                <ul>
                     {
-                        products.map(pd => <Product product={pd}></Product>)
+                        products.map(pd => <Product
+                        handleAddProduct = {handleAddProduct}
+                        product={pd}>
+                        </Product>)
                     }
-                </ul>
             </div>
             <div className="cart-contsiner">
                 <h2>This is cart</h2>
